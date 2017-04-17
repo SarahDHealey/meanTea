3e1920cad64544093eb2d9f66aede629da6ef872
Sarah Healey <s.deane.healey@gmail.com>
2017-04-17 12:23:51 -0600

    added category all and banner and product images for stunners

---
+++ README.md
@@ -0,0 +1,19 @@
+# Stunners
+
+Stunners is a simple shopping cart app. It allows you to view products by category or searchterm. It also enables you to store products in your cart and edit your cart while keeping track of the running total.
+
+## Development notes
+Dependencies: Postgres must already be installed.
+
+ * Clone Repo
+ * $`npm install`
+ * $`nodemon`
+ * Visit http://localhost:3000
+
+
+
+ ![alt text]("/public/images/stunnersshop.gif")
+
+
+
+ 
--- public/javascripts/services/products_factory.js
+++ public/javascripts/services/products_factory.js
@@ -18,124 +18,113 @@ app.factory('ProductsFactory', function() {
     getAllProducts: [
       {
         "_id": "55c8ee82152165d244b98300",
-        "name": "Bayard stew",
-        "ingredients": "concentrated gluten, jewelry, dill, beetle nut, toast",
-        "caffeineScale": 244,
-        "price": 1540,
+        "name": "MODERN ACETATE SQUARE FLAT TOP WRAP SUNGLASSES W/ METAL DETAIL",
+        "description": "Clean and cool modified rectangular wrap frames that are sure to fit your too cool for school attitude. Hand polished finish with silver detail along the arms. Made with an acetate based frame, metal hinges and polycarbonate UV protected lenses.",
+        "price": 1000,
         "inStock": true,
-        "rating": 1,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32664_d?$cimg$",
+        "rating": 5,
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/mensSquare.jpg",
         "__v": 0,
-        "categories": [ "dark", "cold"]
-      },
-
-      {
-        "_id": "55c8ee82152165d244b98301",
-        "name": "Incompactness syrup",
-        "ingredients": "fennel, nutmeg leaves, parsley, cream of 'cream of cream', blarney",
-        "caffeineScale": 49,
-        "price": 7348,
-        "inStock": true,
-        "rating": 2,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
-        "__v": 0,
-        "categories": ["awesome"]
-      },
-      {
-        "_id": "55c8ee82152165d244b98302",
-        "name": "Flexner white tea",
-        "ingredients": "hot sauce, iron, beetle nut, fresco, blarney, raw mashed potato",
-        "caffeineScale": 38,
-        "price": 4991,
-        "inStock": true,
-        "rating": 4,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-        "__v": 0,
-        "categories": ["cold"]
-      },
-      {
-        "_id": "55c8ee82152165d244b98303",
-        "name": "Pressor leaf",
-        "ingredients": "purina chow, flavorings, pepper, acorns, quality tallow, old sock, bay leaf",
-        "caffeineScale": 153,
-        "price": 5496,
-        "inStock": true,
-        "rating": 1,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
-        "__v": 0,
-        "categories": ["dry", "hot", "awesome"]
+        "categories": [ "square"]
       },
       {
         "_id": "55c8ee82152165d244b98304",
-        "name": "Flexner veggie tea",
-        "ingredients": "cream of tartar, eggplant, cake, deer antler",
-        "caffeineScale": 181,
-        "price": 2445,
+        "name": "HIGH FASHION TWO-TONED PANTOS CROSSBAR COLOR MIRROR LENS AVIATOR SUNGLASSES",
+        "description": "Modern architectural construction that pays tribute to 50's and 60's eyewear trends, these pantoscopic frames feature a full metal rim with a glossy inner plastic frame for a contrasting appearance. This pair features an arched bridge design and flashy mirror-coated lenses for a heightened avant-garde expression this season. Made with a metal and plastic based frame, English style nose pieces, and UV400 protected polycarbonate mirrored lens.",
+        "price": 1700,
         "inStock": true,
         "rating": 1,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womenswhitegoldaviator01.jpeg",
         "__v": 0,
-        "categories": ["summer"]
+        "categories": ["aviator"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98301",
+        "name": "OFFICIALLY LICENSED NFL FOOTBALL MIAMI DOLPHINS SPORTS WRAP SUNGLASSES",
+        "description": "Are you ready for some football? Watch the game in style and cheer on Brandon Marshall, Reggie Bush and the rest of the Miami Dolphins with these official NFL shades. Features prominent team logos on the temples and silicone temple grips for easy grip. Also includes a high quality microfiber soft pouch. Made with an acetate based frame, pin-hinges and polycarbonate UV protected lenses.",
+        "price": 1600,
+        "inStock": true,
+        "rating": 2,
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/mensnflmiamiwrap01.jpeg",
+        "__v": 0,
+        "categories": ["sport", "wrap"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98302",
+        "name": "HIGH QUALITY SEMI-RIMLESS RUNNING CYCLING SPORTS WRAP SUNGLASSES",
+        "description": "High quality modern half frame sunglasses that provide a semi-rimless lens for an unobstructed 180 degree view and silicone across the top of the frame so they won't slide down your face. Features an acetate based frame, silicone nose grips, silicone temple grips, inner silicone padding, and UV protected polycarbonate lens",
+        "price": 1700,
+        "inStock": true,
+        "rating": 4,
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/mensredwrap03.jpeg",
+        "__v": 0,
+        "categories": ["sport", "wrap"]
+      },
+      {
+        "_id": "55c8ee82152165d244b98303",
+        "name": "RED BLOODY MARY COCKTAIL PARTY FAVOR DRINKING NOVELTY SUNGLASSES",
+        "description": "Complete with tomatoes, lime and celery, these bloody Mary cocktail sunglasses are the perfect addition to compliment any party or celebration! Great for novelty use, parties, photo booth or as a costume accessory. Made with a plastic based frame, pinned hinges, and polycarbonate UV400 protected lenses.",
+        "price": 1200,
+        "inStock": true,
+        "rating": 1,
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/redbloodymarycocktailsunglasses.jpeg",
+        "__v": 0,
+        "categories": ["novelty"]
       },
       {
         "_id": "55c8ee82152165d244b98305",
-        "name": "Topflighter malt",
-        "ingredients": "botox, toast, cream of 'cream of 'cream of cream'', kitchen scraps, beef, aligator tongue, lawn clippings",
-        "caffeineScale": 241,
-        "price": 4486,
+        "name": "WOMEN'S BOLD METAL BROW BAR SEMI-RIMLESS ROUND CAT EYE SUNGLASSES",
+        "description": "Take your look to the next level with these bold cat eye sunglasses exquisitely crafted with round neutral-hued lenses, a semi-rimless frame, and a high point silhouette for that feminine touch. Slim metal temples and an elegant curved brow bar accent these flirty and ultra-luxurious sunglasses. Made with a plastic and metal based frame, English style nose pieces, metal hinges, and polycarbonate UV400 lenses.",
+        "price": 1100,
         "inStock": true,
         "rating": 3,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31359_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womensblackgoldcateye01.jpg",
         "__v": 0,
-        "categories": ["dry","lucid","warm"]
+        "categories": ["cateye"]
       },
       {
         "_id": "55c8ee82152165d244b98306",
-        "name": "Cooking mix",
-        "ingredients": "flavorings, roasted mushrooms, toast, tumeric",
-        "caffeineScale": 230,
-        "price": 6973,
+        "name": "WOMENS RETRO BOLD COLORED MIRROR OVERSIZED ROUND SUNGLASSES",
+        "description": "Take a trip to the past with these retro-inspired sunglasses designed with a thick round frame and colored mirror lenses that give off 70's vibes. Contrasted with gold-hued metal hinges and nose bridge for a luxurious feel, these fun and flirty sunglasses make the perfect fashion statement. Made with a plastic based frame, reinforced metal hinges, and polycarbonate UV400 lenses.",
+        "price": 1700,
         "inStock": true,
         "rating": 3,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32303_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womenspinkcircle01.jpg",
         "__v": 0,
-        "categories": ["summer"]
+        "categories": ["circle"]
       },
       {
         "_id": "55c8ee82152165d244b98307",
-        "name": "Cooking stew",
-        "ingredients": "eggplant",
-        "caffeineScale": 122,
-        "price": 6003,
+        "name": "WOMENS FASHION THIN METAL CUTE HEART SHAPED SUNGLASSES",
+        "description": "Adorable thin metal heart silhouette frame that are sure to win over hearts on all your sunny days! Stay fun flirty all season long in these metal heart shaped sunglasses. Made with a metal based frame, metal hinges and polycarbonate UV protected lenses.",
+        "price": 1600,
         "inStock": true,
         "rating": 2,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/31358_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womensredheart01.jpeg",
         "__v": 0,
-        "categories": ["dry","winter","lucid"]
+        "categories": ["heart"]
       },
       {
         "_id": "55c8ee82152165d244b98308",
-        "name": "Prevenient herb tea",
-        "ingredients": "cream of tartar, cream of cream, kitchen scraps, flavorings",
-        "caffeineScale": 196,
-        "price": 1374,
+        "name": "PREMIUM HALF FRAME METAL RIVETS HORN RIMMED SUNGLASSES",
+        "description": "Metal Trim and rivets add a sleek and luxurious feel to this classic style. Complete with stylishly wide temples that side high on the frame, these horn rimmed sunglasses come in a variety of bright colors to fit any style. Made with a plastic and metal based frame, English style nose pieces, metal hinges, and polycarbonate UV400 lenses.",
+        "price": 2100,
         "inStock": true,
         "rating": 3,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32174_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womensredwayfarers01.jpg",
         "__v": 0,
-        "categories": ["lucid","hot"]
+        "categories": ["aviator"]
       },
       {
         "_id": "55c8ee82152165d244b98309",
-        "name": "Angular mix",
-        "ingredients": "hot sauce, lawn clippings, fennel, parsley, quinine",
-        "caffeineScale": 196,
+        "name": "WOMENS CAT EYE SUNGLASSES WITH UV400 PROTECTED GRADIENT LENS",
+        "description": "Chic and bold, these oversize cat eye sunglasses express a very 60's and 70's boho inspired look. These sassy cat eyes are the perfect edition to this season's collection. Made with a metal-based frame, metal hinges and UV400 protected lenses.",
         "price": 4158,
         "inStock": true,
         "rating": 2,
-        "imageUrl": "http://s7d5.scene7.com/is/image/Teavana/32621_d?$cimg$",
+        "imageUrl": "https://s3-us-west-1.amazonaws.com/sunglassesshop/sunglassimages/womenstortoiseblueaviators01.jpeg",
         "__v": 0,
-        "categories": ["spring", "warm","winter"]
+        "categories": ["aviator"]
       }
     ]
   }
